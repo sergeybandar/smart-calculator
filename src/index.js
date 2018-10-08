e63701c618069e28aa7e09b3863c7713aa7c5493
sergeybandar <sergeybandar@mail.ru>
2018-10-08 18:42:01 +0300

    smatr-calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,41 @@
 class SmartCalculator {
   constructor(initialValue) {
     // your implementation
+    this.value = String(initialValue);
   }
 
   add(number) {
     // your implementation
+    this.value += '+' + String(number);
+  return this;
   }
   
   subtract(number) {
     // your implementation
+    this.value += '-' + String(number);
+  return this;
   }
 
   multiply(number) {
     // your implementation
+    this.value += '*' + String(number);
+  return this;
   }
 
   devide(number) {
     // your implementation
+    this.value += '/' + String(number);
+  return this;
   }
 
   pow(number) {
     // your implementation
+    this.value += '**' + String(number);
+  return this;
   }
+  valueOf(){
+    return eval(this.value);
+    }
 }
 
 module.exports = SmartCalculator;
